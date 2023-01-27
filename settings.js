ad531ef238f20185c40f744b5c9bfc1410c403a7
RaffiMaker01 <109462898+RaffiMaker01@users.noreply.github.com>
2023-01-27 08:58:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const chalk = require('chalk')
 
 //===> SESUAIKAN DENGAN KEBUTUHAN MU
 global.ownerNumber = ['6285609233482','6285609233482'] //Ganti Jadi Nomer mu Biar Fitur Ownernya Bisa Dipake
-global.Rowner = '6285609233482' //Owner Utama
-global.ownerName = 'Nzrl Afndi' //Ganti Jadi Namamu
-global.packname = 'Subscribe YT' //Ganti Jadi Nama Botmu
-global.author = 'NZRLAFNDI' //Ganti Jadi Namamu
+global.Rowner = '6288212731547' //Owner Utama
+global.ownerName = 'Ari Offc' //Ganti Jadi Namamu
+global.packname = 'MrX-SkY' //Ganti Jadi Nama Botmu
+global.author = 'MrX-SkY' //Ganti Jadi Namamu
 global.prefa = ['','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi' //Saran Gausah Diganti Lagi
 global.webme = 'https://linktr.ee/NzrlAfndi' //Ganti Jadi Bebas Web Sosmed Mu
-global.sosmed = 'https://www.instagram.com/_nzrlafndi' //Ganti Jadi Web Sosmed Mu
+global.sosmed = 'https://www.instagram.com/bangr_xd_shop' //Ganti Jadi Web Sosmed Mu
 
 //====> SARAN GAUSAH DIGANTI SIH DAH PAS SEMUA
 global.limitAwal = {
